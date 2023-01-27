50ad1d9f28b1598abf04a3de2ab237f16d5bb93e
messabih-khalil <msbih.khalil@gmail.com>
2023-01-27 18:04:55 +0100

    create sideBar

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,17 @@
 <template>
-  <router-view />
+  <!-- sidebar -->
+  <SideBar />
+  <!-- app -->
+  <div class="app">
+    <router-view />
+  </div>
 </template>
 
 <script setup>
+// ** imports
+import SideBar from "./components/SideBar.vue";
+
+// **
 import { ref } from "vue";
 
 const name = ref("alaa");
---
+++ src/components/SideBar.vue
@@ -0,0 +1,50 @@
+<script setup>
+//  ** imports
+import clientImage from "../assets/images/client.png";
+</script>
+
+<template>
+  <nav>
+    <!-- logo -->
+    <div class="logo">LG</div>
+    <!-- items -->
+    <div class="items">
+      <a href="">
+        <img :src="clientImage" alt="" />
+      </a>
+    </div>
+  </nav>
+</template>
+
+<style lang="scss" scoped>
+@import "../assets/scss/global.scss";
+
+nav {
+  width: $sidebarWidth;
+  height: 100vh;
+  position: fixed;
+  top: 0;
+  left: 0;
+  background-color: $colorOne;
+
+  .logo {
+    padding: $space-3;
+    text-align: center;
+    font-weight: 900;
+    font-size: $text-xl;
+    color: white;
+  }
+
+  .items {
+    padding: $space-2 $space-3;
+    display: flex;
+    background-color: $colorFive;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    a img {
+      width: 40px;
+    }
+  }
+}
+</style>
