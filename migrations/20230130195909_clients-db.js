0f3d82215532cb14541327203c1147d1ef5de776
messabih-khalil <msbih.khalil@gmail.com>
2023-01-30 21:19:53 +0100

    create models

---
+++ migrations/20230130195909_clients-db.js
@@ -0,0 +1,40 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return (
+    knex.schema
+      //   clients table
+      .createTable("clients", tbl => {
+        tbl.increments("id").primary();
+        tbl.text("name");
+        tbl.string("phone");
+      })
+      // Appointmeents table
+      .createTable("appointments", tbl => {
+        tbl.increments("id").primary();
+        tbl.text("description");
+        tbl.bigInteger("price");
+        tbl.bigInteger("paid");
+        tbl.timestamps(true, true);
+        //   Foreign key with client table
+        tbl.integer("client_id").unsigned().notNullable();
+        tbl
+          .foreign("client_id")
+          .references("clients.id")
+          .onDelete("CASCADE")
+          .onUpdate("CASCADE");
+      })
+  );
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema
+    .dropTableIfExists("clients")
+    .dropTableIfExists("appointments");
+};
