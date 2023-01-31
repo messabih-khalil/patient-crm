e57cb32bad6244a75e6e5373a24975fdc0703c1f
messabih-khalil <msbih.khalil@gmail.com>
2023-01-31 18:26:13 +0100

    create ipc methods

--- ipc/clientsIpc.js
+++ ipc/clientsIpc.js
@@ -2,19 +2,33 @@ const { ipcMain } = require("electron");
 
 // import crud operations
 
-const { addClient } = require("../queries/clientCrud");
+const { addClient, getClients } = require("../queries/clientCrud");
 
 // create client
-const createClientIpc = ipcMain.handle("createClient", (event, payload) => {
-  let data = [];
-  addClient(payload).then(res => {
-    data = res;
-  });
+const createClientIpc = ipcMain.handle(
+  "createClient",
+  async (event, payload) => {
+    let data = {};
+    await addClient(payload).then(res => {
+      data = res[0];
+    });
 
-  return data;
-});
-//
+    return data;
+  }
+);
+
+// get Clients
+
+const getClientsIpc = ipcMain.handle(
+  "getAllClients",
+  async (event, payload) => {
+    let result = [];
+    await getClients().then(res => (result = res));
+    return result;
+  }
+);
 
 module.exports = {
   createClientIpc,
+  getClientsIpc,
 };
